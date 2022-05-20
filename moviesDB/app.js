80e75eb6f33a519d4e4ca873e428429770ba72ad
Esther Boudrenghien <e.boudrenghien@codeur.online>
2022-05-20 13:55:26 +0200

    final moviesDB

--- moviesDB/app.js
+++ moviesDB/app.js
@@ -21,34 +21,58 @@ const movie = new Movie ({
 
 // movie.save()
 
-const HarryPotter = new Movie ({
-    name : "Harry Potter - La coupe de feu",
-    score : 9.5,
-    avis : "Ce film nous fait passer par toutes les émotions"
+const personSchema = new mongoose.Schema ({
+    name: String,
+    age: Number,
+    favouriteMovie: movieSchema
 })
 
-const Totoro = new Movie ({
-    name: "Totoro",
-    score: 3,
-    avis : "Tellement, on m'a parlé de ce film, que je me suis lassée. Je m'attendais à un truc de dingue, mais au final non. L'histoire est prévisible."
+const Person = mongoose.model("Person", personSchema)
+
+const got = new Movie ({
+    name: "Games of Throne",
+    score: 9.5,
+    avis: "Série incroyable, le graphisme, l'histoire est tellement sublime. Attention aux âmes sensibles, il y a pas mal de scène violentes."
 })
 
-const Chihiro = new Movie ({
-    name : "Le voyage de Chihiro",
-    score: 8.5,
-    avis: "Le voyage de Chihiro nous emporte loin, que ce soit niveau émotions, imagination. Je conseille ce film."
-})
-const Troie = new Movie ({
-    name: "Troie",
-    score: 7,
-    avis: "C'est un film intéressant, basée sur l'Histoire réel de Troie. Elle raconte également l'Histoire d'Achille."
+// got.save()
+
+const person = new Person ({
+    name : "Gertrude",
+    age: 78,
+    favouriteMovie : got
 })
 
-const Alexandre = new Movie ({
-    name: "Alexandre",
-    score: 7,
-    avis: "Alexandre est un beau film historique, un peu violent par contre."
-})
+// person.save()
+
+// const HarryPotter = new Movie ({
+//     name : "Harry Potter - La coupe de feu",
+//     score : 9.5,
+//     avis : "Ce film nous fait passer par toutes les émotions"
+// })
+
+// const Totoro = new Movie ({
+//     name: "Totoro",
+//     score: 3,
+//     avis : "Tellement, on m'a parlé de ce film, que je me suis lassée. Je m'attendais à un truc de dingue, mais au final non. L'histoire est prévisible."
+// })
+
+// const Chihiro = new Movie ({
+//     name : "Le voyage de Chihiro",
+//     score: 8.5,
+//     avis: "Le voyage de Chihiro nous emporte loin, que ce soit niveau émotions, imagination. Je conseille ce film."
+// })
+// const Troie = new Movie ({
+//     name: "Troie",
+//     score: 7,
+//     avis: "C'est un film intéressant, basée sur l'Histoire réel de Troie. Elle raconte également l'Histoire d'Achille."
+// })
+
+// const Alexandre = new Movie ({
+//     name: "Alexandre",
+//     score: 7,
+//     avis: "Alexandre est un beau film historique, un peu violent par contre."
+// })
 
 // Movie.insertMany([HarryPotter, Totoro, Chihiro, Troie, Alexandre], (err) => {
 //     if (err) {
@@ -63,13 +87,15 @@ Movie.find((err, movies) => {
         console.log(err);
     } else {
 
-        mongoose.connection.close()
+        // mongoose.connection.close()
         // console.log(movies);
-        movies.forEach((movie) => {
-            console.log(movie.name);
-        })
+        // movies.forEach((movie) => {
+        //     console.log(movie.name);
+        // })
     }
 })
+
+
 const findDocuments = (db, callback) => {
     const collection = db.collection('movies')
     collection.find({}).toArray((err, movies) => {
@@ -80,6 +106,50 @@ const findDocuments = (db, callback) => {
     })
 }
 
+// Movie.deleteOne({_id:"628762776c21c01b380b0b89"}, (err) => {
+//     if (err) {
+//         console.log(err);
+
+//     } else {
+//         console.log("document supprimé");
+//     }
+// })
 
 
+
+
+    // const insertDocuments = (db, callback) => {
+    //     const collection = db.collection('movies')
+    //     collection.insertMany([
+    //         {
+    //             name: "Le seigneur des anneaux - trilogie - version longue",
+    //             score: 10/10,
+    //             avis : "Une trilogie sublime, drôle."
+            
+    //         }, 
+    //         {
+    //             name: "Harry Potter - La coupe de feu",
+    //             score: 9.5/10,
+    //             avis : "Ce film nous fait passer par toutes les émotions"
+            
+    //         }, 
+    //         {
+    //             name: "Totoro",
+    //             score: 3/10,
+    //             avis : "Tellement, on m'a parlé de ce film, que je me suis lassée. Je m'attendais à un truc de dingue, mais au final non. L'histoire est prévisible."
+            
+    //         },
+    //         {
+    //             name: "Le voyage de Chihiro",
+    //             score: 8.5/10,
+    //             avis: "Le voyage de Chihiro nous emporte loin, que ce soit niveau émotions, imagination. Je conseille ce film."
+    //         }
+    //     ], ((err, result) => {
+    //         assert.equal(err, null) // cette commande permet de savoir si il n'y a pas d'erreurs lors de l'insertion des documents.
+    //         assert.equal(4,result.insertedCount);
+    //         assert.equal(4,Object.keys(result.insertedIds).length);
+    //         console.log("insertion de 4 documents dans la collection");
+    //         callback(result)
+    //     }))
+    // }
 
